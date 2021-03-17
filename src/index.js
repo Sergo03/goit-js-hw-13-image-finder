c8c876ec713782c63bff5b5ec1e8569b80e7533b
Sergey Karpynskyi <karpinskiy94@gmail.com>
2021-03-17 19:06:56 +0200

    hw13(123)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import './styles.css';
 import ImgApi from './apiService';
+
+const basicLightbox = require('basiclightbox')
 var debounce = require('lodash.debounce');
 
 const searchRef = document.querySelector('#search-form');
 const btnRef = document.querySelector('.load-more');
 const listImgRef = document.querySelector('.gallery');
+
+
+
+
 const imgApi = new ImgApi();
 
 searchRef.addEventListener('input', debounce(onSearch, 500));
 btnRef.addEventListener('click', onClick);
 btnRef.addEventListener('click', scroll);
 
+
 function onSearch(event) {
+    listImgRef.innerHTML = '';
     imgApi.query = event.target.value;
     imgApi.resetPage();
     imgApi.fetchImg(imgApi.query);
-    
 }
+
+
 
 function onClick() {
     imgApi.fetchImg(imgApi.query);
